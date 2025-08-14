6d1e441ef89462ea08aae3fa180f23c5edd0e4cf
viper8002z <viper8002z@gmail.com>
2025-08-14 21:19:51 +0330

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
-    import asyncio
-    import websockets
+import asyncio
+import websockets
 
-    async def echo(websocket):
-        async for message in websocket:
-            print(f"Received from client: {message}")
-            await websocket.send(f"Server echoed: {message}")
-            print(f"Sent back to client: Server echoed: {message}")
+async def echo(websocket):
+    async for message in websocket:
+        print(f"Received from client: {message}")
+        await websocket.send(f"Server echoed: {message}")
+        print(f"Sent back to client: Server echoed: {message}")
 
-    async def main():
-        async with websockets.serve(echo, "localhost", 8765):
-            print("WebSocket server started on ws://localhost:8765")
-            await asyncio.Future()
+async def main():
+    async with websockets.serve(echo, "localhost", 8765):
+        print("WebSocket server started on ws://localhost:8765")
+        await asyncio.Future()
 
-    if __name__ == "__main__":
-        asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
